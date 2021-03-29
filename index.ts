92bd73332ad9db19dcdaa0e7a130bbc0933895f0
Mir Ashif <mir.ashif@outlook.com>
2021-03-30 02:29:48 +0600

    1.0.0 release

---
+++ index.ts
@@ -0,0 +1,9 @@
+const isEmpty = (data: any): boolean => {
+    if (Array.isArray(data)) return !data.length;
+
+    else if (typeof data === 'object' && data !== null) return !Object.keys(data).length;
+    
+    else return !data
+}
+
+export default isEmpty;
