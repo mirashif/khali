b8444d3803975ac90e9b786e9a1aad412ea912ef
Mir Ashif <mirashif01@gmail.com>
2024-05-21 20:20:43 +0600

    release v2
    rm bun
    add vitest
    update pkg config

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,108 +1,108 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, test } from "vitest";
 
 import isEmpty from "../src/index";
 
 describe("Truthy cases", () => {
-	test("empty array", () => {
-		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		const value: any[] = [];
-		expect(isEmpty(value)).toBeTruthy();
-	});
+  test("empty array", () => {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    const value: any[] = [];
+    expect(isEmpty(value)).toBeTruthy();
+  });
 
-	test("empty object", () => {
-		const value = {};
-		expect(isEmpty(value)).toBeTruthy();
-	});
+  test("empty object", () => {
+    const value = {};
+    expect(isEmpty(value)).toBeTruthy();
+  });
 
-	test("empty string", () => {
-		const value = "";
-		expect(isEmpty(value)).toBeTruthy();
-	});
+  test("empty string", () => {
+    const value = "";
+    expect(isEmpty(value)).toBeTruthy();
+  });
 
-	test("number zero", () => {
-		const value = 0;
-		expect(isEmpty(value)).toBeTruthy();
-	});
+  test("number zero", () => {
+    const value = 0;
+    expect(isEmpty(value)).toBeTruthy();
+  });
 
-	test("non-existing array item", () => {
-		const value = ["Dhaka", ["Uttara", [1, 2]]];
-		expect(isEmpty(value[10])).toBeTruthy();
-	});
+  test("non-existing array item", () => {
+    const value = ["Dhaka", ["Uttara", [1, 2]]];
+    expect(isEmpty(value[10])).toBeTruthy();
+  });
 
-	test("non-existing object item", () => {
-		const value = {
-			city: "Dhaka",
-			location: {
-				latitude: "23.809473999508782",
-				longitude: "90.4151957081839",
-			},
-		};
-		// @ts-ignore
-		expect(isEmpty(value.capital)).toBeTruthy();
-	});
+  test("non-existing object item", () => {
+    const value = {
+      city: "Dhaka",
+      location: {
+        latitude: "23.809473999508782",
+        longitude: "90.4151957081839",
+      },
+    };
+    // @ts-ignore
+    expect(isEmpty(value.capital)).toBeTruthy();
+  });
 
-	test("non-existing nested array item", () => {
-		const value = ["Dhaka", ["Uttara", [1, 2]]];
-		// @ts-ignore
-		expect(isEmpty(value[0][100])).toBeTruthy();
-	});
+  test("non-existing nested array item", () => {
+    const value = ["Dhaka", ["Uttara", [1, 2]]];
+    // @ts-ignore
+    expect(isEmpty(value[0][100])).toBeTruthy();
+  });
 
-	test("non-existing nested object item", () => {
-		const value = {
-			city: "Dhaka",
-			location: {
-				latitude: "23.809473999508782",
-				longitude: "90.4151957081839",
-			},
-		};
-		// @ts-ignore
-		expect(isEmpty(value.location.area)).toBeTruthy();
-	});
+  test("non-existing nested object item", () => {
+    const value = {
+      city: "Dhaka",
+      location: {
+        latitude: "23.809473999508782",
+        longitude: "90.4151957081839",
+      },
+    };
+    // @ts-ignore
+    expect(isEmpty(value.location.area)).toBeTruthy();
+  });
 });
 
 describe("Falsy cases", () => {
-	test("negative number", () => {
-		const value = -1;
-		expect(isEmpty(value)).toBeFalsy();
-	});
+  test("negative number", () => {
+    const value = -1;
+    expect(isEmpty(value)).toBeFalsy();
+  });
 
-	test("positive number", () => {
-		const value = 1;
-		expect(isEmpty(value)).toBeFalsy();
-	});
+  test("positive number", () => {
+    const value = 1;
+    expect(isEmpty(value)).toBeFalsy();
+  });
 
-	test("array item", () => {
-		const value = ["Dhaka", ["Uttara", [1, 2]]];
-		expect(isEmpty(value[0])).toBeFalsy();
-	});
+  test("array item", () => {
+    const value = ["Dhaka", ["Uttara", [1, 2]]];
+    expect(isEmpty(value[0])).toBeFalsy();
+  });
 
-	test("object item", () => {
-		const value = {
-			city: "Dhaka",
-			location: {
-				latitude: "23.809473999508782",
-				longitude: "90.4151957081839",
-			},
-		};
-		// @ts-ignore
-		expect(isEmpty(value.city)).toBeFalsy();
-	});
+  test("object item", () => {
+    const value = {
+      city: "Dhaka",
+      location: {
+        latitude: "23.809473999508782",
+        longitude: "90.4151957081839",
+      },
+    };
+    // @ts-ignore
+    expect(isEmpty(value.city)).toBeFalsy();
+  });
 
-	test("nested array item", () => {
-		const value = ["Dhaka", ["Uttara", [1, 2]]];
-		// @ts-ignore
-		expect(isEmpty(value[0][0])).toBeFalsy();
-	});
+  test("nested array item", () => {
+    const value = ["Dhaka", ["Uttara", [1, 2]]];
+    // @ts-ignore
+    expect(isEmpty(value[0][0])).toBeFalsy();
+  });
 
-	test("nested object item", () => {
-		const value = {
-			city: "Dhaka",
-			location: {
-				latitude: "23.809473999508782",
-				longitude: "90.4151957081839",
-			},
-		};
-		// @ts-ignore
-		expect(isEmpty(value.location.latitude)).toBeFalsy();
-	});
+  test("nested object item", () => {
+    const value = {
+      city: "Dhaka",
+      location: {
+        latitude: "23.809473999508782",
+        longitude: "90.4151957081839",
+      },
+    };
+    // @ts-ignore
+    expect(isEmpty(value.location.latitude)).toBeFalsy();
+  });
 });
